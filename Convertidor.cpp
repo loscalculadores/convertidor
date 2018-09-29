8b9ba92a419ee3e08f04a0ad267ea8e372925cd3
Carlos Isaac Casco Leguizamon <carlin_isaac@hotmail.com>
2018-09-29 12:55:43 -0400

    Inicio del codigo y opciones del switch

--- Convertidor.cpp
+++ Convertidor.cpp
@@ -1,8 +1,21 @@
-#include<conio.h>
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+
 
 int main(){
-	system("color a1");
-	printf("\n\nConvertidor");
+	system("color 3f");
+	int n,aux,dec=0,dig,c=0,opc,bin=0;
+	printf("\n\n\tCONVERTIDOR DE N\243MEROS\n ");
+	printf("\n\n\tSeleccione el Tipo de numero que Deseas introducir: ");
+	printf("\n\t1.Decimal ");
+	printf("\n\t2.Binario ");
+	printf("\n\t3.Octal ");
+	printf("\n\t4.Hexadecimal\n ");
+	scanf("%d",&opc);
+	
 }
+	
+
+
