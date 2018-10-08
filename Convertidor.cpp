ea14e1844263c1857ae547c9a42f2b067b99d6fd
VidalMelgarejo11 <43355514+VidalMelgarejo11@users.noreply.github.com>
2018-10-08 13:36:30 -0300

    Update Convertidor.cpp
    convertir de decimal a octal y hexadecimal

--- Convertidor.cpp
+++ Convertidor.cpp
@@ -16,6 +16,30 @@ int main(){
 	printf("\n\t4.Hexadecimal\n ");
 	scanf("%d",&opc);
 	
+	switch(opc){
+		case 1: 
+	
+	    	//Caso 1 trabajamos con los decimales!!
+	    	//Para Convertir de Decimal a Binario
+		 printf("\n\n\tUsted a Seleccionado un Numero Decimal ");
+	     printf("\n\nIngrese el Numero:\t");
+	     scanf("%d",&n);
+       	 c=1;
+	    aux=n;
+	    while(aux>=1){
+		dig=aux%2;
+		aux=aux/2;
+		bin=bin+dig*c;
+		c=c*10;	
+	}
+	printf("\n\n%d en Binario es %d",n,bin);
+	//Para Convertir de Decimal a Octal y Hexadecimal
+    int x;
+	x=n;
+	printf("\n\n%d en Octal es:  %o",x,x);
+	printf("\n\n%d en Hexadecimal es: %X",x,x);
+		break;
+	
 =======
 	system("color a1");
 	printf("\n\nConvertidor");
@@ -23,5 +47,3 @@ int main(){
 >>>>>>> 5cca0236ca15af8442a7e70a9d64751367b714f3
 }
 	
-
-
