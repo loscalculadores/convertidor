1ac3f5b70d38ff688e860ea5dcc90d2c6a754ad4
Superkemda <42779009+Superkemda@users.noreply.github.com>
2018-10-08 13:45:34 -0300

    Update Convertidor.cpp
    convertir de octal a los otros

--- Convertidor.cpp
+++ Convertidor.cpp
@@ -21,10 +21,10 @@ int main(){
 	
 	    	//Caso 1 trabajamos con los decimales!!
 	    	//Para Convertir de Decimal a Binario
-		 printf("\n\n\tUsted a Seleccionado un Numero Decimal ");
-	     printf("\n\nIngrese el Numero:\t");
-	     scanf("%d",&n);
-       	 c=1;
+		printf("\n\n\tUsted a Seleccionado un Numero Decimal ");
+	     	printf("\n\nIngrese el Numero:\t");
+	     	scanf("%d",&n);
+       	 	c=1;
 	    aux=n;
 	    while(aux>=1){
 		dig=aux%2;
@@ -34,18 +34,18 @@ int main(){
 	}
 	printf("\n\n%d en Binario es %d",n,bin);
 	//Para Convertir de Decimal a Octal y Hexadecimal
-    int x;
+    	int x;
 	x=n;
 	printf("\n\n%d en Octal es:  %o",x,x);
 	printf("\n\n%d en Hexadecimal es: %X",x,x);
 		break;
 		case 2: 
-			//Caso 2
-		   //Trabajamos con los binarios
-		  //Para Convertir de Binario a decimal
+		//Caso 2
+		//Trabajamos con los binarios
+		//Para Convertir de Binario a decimal
 		printf("\n\n\tUsted a Seleccionado un numero Binario");
-	printf("\n\nIngrese el Numero\t:");
-	    scanf("%d",&n);
+		printf("\n\nIngrese el Numero\t:");
+	    	scanf("%d",&n);
 		aux=n;
 	    while(aux>0){
 		dig=aux%10;
@@ -59,6 +59,38 @@ int main(){
 			printf("\n\n%d en Octal es:  %o",n,x);
 	        printf("\n\n%d es Hexadecimal es: %X",n,x);	
 	    break;
+	    case 3:
+		// Caso 3
+		     //Trabajaremos con los Numeros de Base Octal
+		     // De Octal convertimos a decimal
+		     printf("\n\n\tUsted a Seleccionado un numero Octal");
+		   		printf("\n\nIngrese el Numero\t:");
+		    scanf("%d",&n);
+		    int octal;
+			aux=n;
+		    while(aux>0){
+			dig=aux%10;
+			octal=octal+dig*pow(8,c);
+			aux=aux/10;
+			c++;
+			}
+			//Una vez convertido en decimal pasemos a octal y hexadecimal
+			x=octal;
+			printf("\n\n%d en Decimal es: %d",n,octal);
+		    printf("\n\n%d en Hexadecimal es: %X",n,x);	
+		   	
+		   	//una vez convertido en decimal pasemoslo a binario
+		   	c=1;
+		    aux=octal;
+		    while(aux>=1){
+			dig=aux%2;
+			aux=aux/2;
+			bin=bin+dig*c;
+			c=c*10;	
+		}
+		   	  printf("\n\n%d en Binario es: %d",n,bin);	
+	   	    	
+	   	break;
 	
 =======
 	system("color a1");
